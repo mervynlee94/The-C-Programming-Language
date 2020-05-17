676fc21f81c1abbad99de38e9ec63db41c64f312
Mervyn Lee <mervynlee1994@gmail.com>
2020-05-17 17:57:26 +0800

    Exercise 1-8 submission

---
+++ ch01-a-tutorial-introduction/exercise1-8.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *  Created:  17/05/2020 
+ *  Compiler:  
+ *      Apple clang version 11.0.3 (clang-1103.0.32.29)
+ *      Target: x86_64-apple-darwin19.4.0
+ *      Thread model: posix
+ * 
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(){
+   int c, nl, nb, nt;
+   
+   nl = nb = nt = 0;
+   while((c=getchar()) != EOF){
+       if (c == '\n')
+            ++nl;
+        else if (c == '\t')
+            ++nt;
+        else if (c == ' ')
+            ++nb;
+    }
+   printf("Newline: %d\nTab: %d\nSpace: %d", nl, nt, nb);
+}
