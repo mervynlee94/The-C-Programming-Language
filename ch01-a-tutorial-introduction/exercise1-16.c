5f941d6c4054b5ce10d46df5cf91a52c40b4f1f9
Mervyn Lee <mervynlee1994@gmail.com>
2020-05-18 13:33:11 +0800

    Revisit to fix logic

--- ch01-a-tutorial-introduction/exercise1-16.c
+++ ch01-a-tutorial-introduction/exercise1-16.c
@@ -39,17 +39,19 @@ int main(){
 int getLine(char s[], int lim){
     int c, i;
     for(i = 0 ; i < lim-1 && (c=getchar()) != EOF && c!= '\n'; ++i)
-        if (i < lim)
-            s[i] = c;
-        
-    if(c == '\n'){
-        s[i] = c;
-        ++i;
+        s[i]=c;
+    if(i == (lim-1)){
+        s[i] = '\0';
+        while((c=getchar()) != EOF && c !='\n')
+            ++i;
     }
-    s[i] = '\0';
-    while((c=getchar()) != EOF)
-        ++i;
-    
+    else{
+        if(c == '\n'){
+            s[i] = c;
+            ++i;
+        }
+        s[i] = '\0';
+    }
     return i;
 }
 
