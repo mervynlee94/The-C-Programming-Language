6d000b86458250d1cdf8a4a5f49e017125a547fd
Mervyn Lee <mervynlee1994@gmail.com>
2020-06-08 18:36:12 +0800

    Exercise 5-04 submission

---
+++ ch05-pointers-and-arrays/exercise5-03.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *  Created:  08/06/2020 
+ *  Compiler:  
+ *      Apple clang version 11.0.3 (clang-1103.0.32.29)
+ *      Target: x86_64-apple-darwin19.4.0
+ *      Thread model: posix
+ *  Description: Write a pointer version of the function strcat that we showed in Chapter 2:
+    strcat(s,t) copies the string t to the end of s.
+ * 
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#define MAXLENGTH 100
+void mystrcat(char *s, char *t);
+int main(){
+    char s[MAXLENGTH] = "Hello";
+    char t[] = " World!";
+    mystrcat(s, t);
+    printf("%s", s);
+    return 0;
+}
+
+void mystrcat(char *s, char *t) {
+    while(*s++)
+        if(*s == '\0')
+            while((*s++ = *t++))
+                ;
+}
